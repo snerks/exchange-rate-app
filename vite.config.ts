ae29182593d9cab64a96d00f3e9f03b2059b2b78
snerks <martin.github@outlook.com>
2025-06-08 10:50:06 +0100

    Restore development proxy

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,19 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/ecb-xml": {
-  //       target: "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml",
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/ecb-xml/, ""),
-  //       // Remove path so /ecb-xml -> /
-  //       configure: (proxy, _) => {
-  //         proxy.on("proxyReq", (proxyReq, __, ___) => {
-  //           proxyReq.path = "/stats/eurofxref/eurofxref-daily.xml";
-  //         });
-  //       },
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/ecb-xml": {
+        target: "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ecb-xml/, ""),
+        // Remove path so /ecb-xml -> /
+        configure: (proxy, _) => {
+          proxy.on("proxyReq", (proxyReq, __, ___) => {
+            proxyReq.path = "/stats/eurofxref/eurofxref-daily.xml";
+          });
+        },
+      },
+    },
+  },
 });
