868b35b2e31c8858a240cf97ab9a9c5bb7be7df9
snerks <martin.github@outlook.com>
2025-06-08 11:13:52 +0100

    Add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/exchange-rate-app",
   server: {
     proxy: {
       "/ecb-xml": {
